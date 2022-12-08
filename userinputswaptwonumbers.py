c57c9e7b772acce9d3183b7bb10f786d080f2269
umashankar <84461244+Sivaumashankar@users.noreply.github.com>
2022-12-08 19:08:16 +0530

    Add files via upload

---
+++ userinputswaptwonumbers.py
@@ -0,0 +1,11 @@
+num1=input("enter num1 value")
+num2=input("enter num2 value")
+print("value of num1 before swapping",num1)
+print("value of num2 before swapping",num2)
+temp=num1 #10
+num1=num2  #20
+num2=temp #10
+print("value of num1 after swapping",num1)
+print("value of num2 after swapping",num2)
+
+
