9cb4ef41032c47ee1891614f5d8a80eac306b347
umashankar <84461244+Sivaumashankar@users.noreply.github.com>
2023-01-01 21:34:35 +0530

    Add files via upload

---
+++ Sqlassignment/Query1_2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE EmployeeDetails(emp_id int primary key, 
+emp_name varchar(20) not null,
+job_name varchar(20) not null,
+manager_id int not null ,
+ hire_date date not null,
+salary int not null,
+Dep_id int not null);
---
+++ Sqlassignment/Query1_5.sql
@@ -0,0 +1,1 @@
+select distinct job_name as Desgination from EmployeeDetails;
